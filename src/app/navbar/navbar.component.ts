9801f2ac70cb92535e1350c42a7d033a6714054a
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-05-10 00:54:01 +0200

    Scrolling function added.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  activeLink: string | null = 'about';
 
-  constructor() { }
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const yOffset = window.pageYOffset;
 
-  ngOnInit(): void {
+    const sections = [
+      'about',
+      'skills',
+      'projects',
+      'resume',
+      'contact'
+    ];
+
+    for (const section of sections) {
+      const element = document.getElementById(section);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top <= yOffset + 100 && rect.bottom > yOffset + 100) {
+          this.activeLink = section;
+          break;
+        }
+      }
+    }
   }
 
+  scrollToElement(event: MouseEvent, elementId: string): void {
+    event.preventDefault();
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({behavior: 'smooth'});
+    }
+  }
 }
