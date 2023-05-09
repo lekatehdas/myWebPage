d03274b6a6f5263b827db572018815e1f4077437
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-05-10 00:24:01 +0200

    First draft of skills done.

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,22 +1,31 @@
-import { trigger, transition, style, animate, query } from '@angular/animations';
+import {trigger, transition, style, animate, query, stagger} from '@angular/animations';
 
 export const fadeInFromLeft = trigger('fadeInFromLeft', [
   transition(':enter', [
-    style({ opacity: 0, transform: 'translateX(-100%)' }),
-    animate('1500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+    style({opacity: 0, transform: 'translateX(-100%)'}),
+    animate('1500ms ease-out', style({opacity: 1, transform: 'translateX(0)'}))
   ])
 ]);
 
 export const fadeInFromRight = trigger('fadeInFromRight', [
   transition(':enter', [
-    style({ opacity: 0, transform: 'translateX(100%)' }),
-    animate('1500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+    style({opacity: 0, transform: 'translateX(100%)'}),
+    animate('1500ms ease-out', style({opacity: 1, transform: 'translateX(0)'}))
   ])
 ]);
 
 export const fadeInFromRightDelayed = trigger('fadeInFromRightDelayed', [
   transition(':enter', [
-    style({ opacity: 0, transform: 'translateX(100%)' }),
-    animate('1500ms 400ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+    style({opacity: 0, transform: 'translateX(100%)'}),
+    animate('1500ms 400ms ease-out', style({opacity: 1, transform: 'translateX(0)'}))
   ])
+]);
+
+export const fadeInAnimation = trigger('fadeIn', [
+  transition('* => *', [
+    query('.skill', [
+      style({opacity: 0}),
+      stagger(100, [animate('500ms', style({opacity: 1}))]),
+    ], {optional: true}),
+  ]),
 ]);
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fadeInAnimation} from "../animations/animations";
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
+  animations: [fadeInAnimation]
 })
 export class SkillsComponent implements OnInit {
+  currentSkill = -1;
+  skills = [
+    {
+      title: 'Programming Languages',
+      items: ['Python', 'Typescript', 'JavaScript', 'Java', 'Kotlin']
+    },
+    {
+      title: 'Web Development',
+      items: ['HTML', 'CSS', 'Angular', 'Node.js', 'Express', 'Django']
+    },
+    {
+      title: 'Databases',
+      items: ['MySQL', 'MongoDB', 'Firebase', 'Firestore']
+    },
+    {
+      title: 'Big Data and Machine Learning',
+      items: ['Scikit-learn', 'TensorFlow', 'Hadoop', 'Spark']
+    },
+    {
+      title: 'Software Engineering',
+      items: ['Agile Methodologies', 'Design Patterns', 'Git', 'JIRA', 'Testing', 'Version control (GIT)']
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
 }
