aea26012a67b0f780d77187f573e582ecdc07cec
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-05-14 23:18:47 +0200

    Mobile about resized.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,7 +4,7 @@ import { fadeInFromLeft, fadeInFromRight, fadeInFromRightDelayed } from '../anim
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'],
+  styleUrls: ['./about.component.css', "./about.mobile.css"],
   animations: [fadeInFromLeft, fadeInFromRight, fadeInFromRightDelayed]
 })
 export class AboutComponent implements OnInit {
