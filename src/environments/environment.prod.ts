346b2be53a112d9999291a8717882b20eaf6816f
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-05-15 13:52:16 +0200

    Minor style changes.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  discordWebhook: 'https://discord.com/api/webhooks/1107280140792254526/oFHfpzemVjsp8SgsWm5O_EBWy6Ak-O1qyZAciBth4VNmbPVYlL0e4Pe0dqYjJqbuE0wg'
 };
